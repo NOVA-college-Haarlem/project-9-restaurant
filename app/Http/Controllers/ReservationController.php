95215094546580ae144466df51d37b5bca77f1ff
lorenzo <lorenzoschemmekes@gmail.com>
2025-03-31 09:36:37 +0200

    made crud reservations

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
+    public function index()
+    {
+        $reservations = Reservation::all();
+        return view('reservations.index', compact('reservations'));
+    }
+
     public function create()
     {
-        return view('reservation.blade.php');
+        return view('Reservations.create');
     }
 
     public function store(Request $request)
@@ -19,8 +26,38 @@ class ReservationController extends Controller
             'requests' => 'nullable|string',
         ]);
 
+        Reservation::create($request->all());
 
+        return redirect()->route('reservations.index')->with('success', 'Reservering succesvol ontvangen!');
+    }
 
-        return redirect()->route('reservation.blade.php')->with('success', 'Reservering succesvol ontvangen!');
+    public function edit(Reservation $reservation)
+    {
+        return view('reservations.edit', compact('reservation'));
+    }
+
+    public function update(Request $request, Reservation $reservation)
+    {
+        $request->validate([
+            'date' => 'required|date',
+            'guests' => 'required|integer|min:1',
+            'requests' => 'nullable|string',
+        ]);
+
+        $reservation->update($request->all());
+
+        return redirect()->route('reservations.index')->with('success', 'Reservering bijgewerkt!');
+    }
+
+    public function destroy(Reservation $reservation)
+    {
+        $reservation->delete();
+        return redirect()->route('reservations.index')->with('success', 'Reservering geannuleerd!');
+    }
+
+    public function calendar()
+    {
+        $reservations = Reservation::all();
+        return view('reservations.calendar', compact('reservations'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\UserController;
 
-Route::get('/reserveren', [ReservationController::class, 'create'])->name('reservations.create');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/create', [ReservationController::class, 'create'])->name('reservations.create');
 Route::post('/reserveren', [ReservationController::class, 'store'])->name('reservations.store');
+Route::resource('reservations', ReservationController::class);
+Route::get('/calendar', [ReservationController::class, 'calendar'])->name('reservations.calendar');
+
+
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
